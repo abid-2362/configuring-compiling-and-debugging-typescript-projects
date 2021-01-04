a1233e92444e944a27374e1d8bc267e72390a55b
Abid Ali <abdsoftfsd@gmail.com>
2021-01-04 09:14:05 +0500

    Adds 2 npm commands to run build and dev

--- server/server.ts
+++ server/server.ts
@@ -7,5 +7,5 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`listening to port ${port}`);
+  console.log(`listening the server on port ${port}`);
 });
